d2f3ad2caed95031faafc8e8b04e0394fde01aa0
Jacob Brooks <myrefugemyfortress@yahoo.com>
2021-12-26 22:54:47 -0500

    finished main work on migrations

--- data/migrations/20211227000655_projects.js
+++ data/migrations/20211227000655_projects.js
@@ -2,7 +2,9 @@
 exports.up = function(knex) {
     return knex.schema.createTable("projects",table=>{
         table.increments("project_id")
-        table.text("name",128).unique().notNullable()
+        table.text("project_name",128).unique().notNullable()
+        table.text("project_description")
+        table.boolean("project_completed")
     })
 };
 
--- data/migrations/20211227000728_resources.js
+++ data/migrations/20211227000728_resources.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("resources",table=>{
+        table.increments("resource_id")
+        table.text("resource_name",128).unique().notNullable()
+        table.text("resource_description")
+    })
 };
 
 exports.down = function(knex) {
-  
+   return knex.schema.dropTableIfExists("resources")
 };
